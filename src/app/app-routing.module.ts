7f020b2b53ba37e7d3aecd622b125b867542d128
Len Paalvast <l.paalvast@portbase.com>
2018-10-30 21:54:41 +0100

    router + outlet toegevoegd... bezig met centreren van svg + outline toevoegen

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,29 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {RouterModule, Routes} from '@angular/router';
+import {LobbyComponent} from './lobby/lobby.component';
+import {MarsMapComponent} from './mars-map/mars-map.component';
+import {HomeMenuComponent} from './home-menu/home-menu.component';
+
+const routes: Routes = [
+    {
+      path: '', component: HomeMenuComponent
+    },
+    {
+        path: 'lobby', component: LobbyComponent
+    },
+    {
+        path: 'mars-map', component: MarsMapComponent
+    }
+];
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+    imports: [
+        CommonModule,
+        RouterModule.forRoot(routes)
+    ],
+    declarations: [],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,10 @@ import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import {MarsMapComponent} from './mars-map/mars-map.component';
 import {LobbyComponent} from './lobby/lobby.component';
-import {MaterialModule} from './material/material.module';
-import { MenuComponent } from './menu/menu.component';
-import { HomeMenuComponent } from './home-menu/home-menu.component';
-import { AppRoutingModule } from './/app-routing.module';
+import {MaterialModule} from './material.module';
+import {MenuComponent} from './menu/menu.component';
+import {HomeMenuComponent} from './home-menu/home-menu.component';
+import {AppRoutingModule} from './/app-routing.module';
 
 
 @NgModule({
